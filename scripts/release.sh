1b43691b06402e6da35bddd64d7e876f17805b2c
Leonid Makarov <leonid.makarov@ffwagency.com>
2018-11-21 23:04:27 -0800

    Fix tags in "make release" script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-	# develop => edge
-	[[ "${TRAVIS_BRANCH}" == "develop" ]] && TAG="edge"
-	# master => latest
-	[[ "${TRAVIS_BRANCH}" == "master" ]] && TAG="latest"
-	# tags/v1.2.0 => 1.2
-	[[ "${TRAVIS_TAG}" != "" ]] && TAG="${TRAVIS_TAG:1:3}"
+	# develop => edge-${VERSION}
+	[[ "${TRAVIS_BRANCH}" == "develop" ]] && TAG="edge-${VERSION}"
+	# master => ${VERSION}
+	[[ "${TRAVIS_BRANCH}" == "master" ]] && TAG="${VERSION}"
+	# tags/v1.2.0 => 1.2-${VERSION}
+	[[ "${TRAVIS_TAG}" != "" ]] && TAG="${TRAVIS_TAG:1:3}-${VERSION}"
 
 	if [[ "$TAG" != "" ]]; then
 		docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
